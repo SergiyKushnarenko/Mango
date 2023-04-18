0460b0194dc771c9184fdb9ccff60221f120058e
SerhiiKushanrenko <108808018+SerhiiKushanrenko@users.noreply.github.com>
2023-04-18 12:23:42 +0300

    Create BaseService

---
+++ Mongo.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Models;
+using Mongo.Web.Models;
+
+namespace Mongo.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}

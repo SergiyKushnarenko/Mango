ee50addcfb17876144b18e48e827a25174ebc34c
SerhiiKushanrenko <108808018+SerhiiKushanrenko@users.noreply.github.com>
2023-04-19 11:47:02 +0300

    add identity Service project

---
+++ Mango.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.Identity.DbContexts
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql();
+		}
+
+	}
+}

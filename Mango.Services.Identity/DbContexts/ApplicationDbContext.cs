8ac15f47e4edf99dc5cb99f1b4ea282e31029bc2
SerhiiKushanrenko <108808018+SerhiiKushanrenko@users.noreply.github.com>
2023-04-20 09:59:48 +0300

    create db in IdentityServer

--- Mango.Services.Identity/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.Identity.DbContexts
 {
-	public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
